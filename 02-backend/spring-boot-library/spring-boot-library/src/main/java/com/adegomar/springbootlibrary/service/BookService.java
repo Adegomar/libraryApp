329cf32bf039baaa68266badb05f3c0b43abbccb
Adrian Kocyk <adegomar123@gmail.com>
2023-03-02 16:22:10 +0100

    1st Commit

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/adegomar/springbootlibrary/service/BookService.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/adegomar/springbootlibrary/service/BookService.java
@@ -103,4 +103,21 @@ public class BookService {
         return shelfCurrentLoansResponses;
     }
 
+    public void returnBook (String userEmail, Long bookId) throws Exception{
+
+        Optional<Book> book = bookRepository.findById(bookId);
+// veryfying that the database within our checkout table we have, have a record that has our userEmail with the specific book ID
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if(!book.isPresent() || validateCheckout == null){
+            throw new Exception("Book does not exist or not checked out by user");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+
+        bookRepository.save(book.get());
+        checkoutRepository.deleteById(validateCheckout.getId());
+
+    }
+
 }
