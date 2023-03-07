3369d4d8d5fe3ca1153ad3162e636c9013d487e8
Adrian Kocyk <adegomar123@gmail.com>
2023-03-07 20:09:23 +0100

    Admin book options

--- 03-frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
+++ 03-frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -39,6 +39,16 @@ export const Navbar = () => {
                                 <NavLink className='nav-link' to='/shelf'>Shelf</NavLink>
                             </li>
                         }
+                        {authState.isAuthenticated &&
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to='/messages'>Q/A</NavLink>
+                            </li>
+                        }
+                        {authState.isAuthenticated && authState.accessToken?.claims?.userType === 'admin' &&
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to='/admin'>Admin</NavLink>
+                            </li>
+                        }
                     </ul>
                     <ul className='navbar-nav ms-auto'>
                         {!authState.isAuthenticated ?
